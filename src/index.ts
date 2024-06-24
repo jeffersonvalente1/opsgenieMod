9231b16faa4993947e94ed68f63a2465cd8d0ec2
jeffersonvalente1 <jefferson.valente@platformbuilders.io>
2024-06-24 10:25:37 -0300

    bora

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export * from './api';
 export * from './integration';
 export { Layout } from './components/OpsgeniePage';
 export { AlertsList } from './components/AlertsTable';
-export { IncidentsList } from './components/IncidentsTable';
+//export { IncidentsList } from './components/IncidentsTable';
 export * from './components/OnCallList';
 export { Analytics } from './components/Analytics';
 export { AlertsSummary } from './components/AlertsSummary';
