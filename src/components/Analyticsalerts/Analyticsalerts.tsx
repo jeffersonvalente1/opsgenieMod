d4f958ccb9b807b850a5bbbac1e927b9961e5610
jeffersonvalente1 <jefferson.valente@platformbuilders.io>
2024-07-04 10:47:28 -0300

    added teamsId 6

--- src/components/Analyticsalerts/Analyticsalerts.tsx
+++ src/components/Analyticsalerts/Analyticsalerts.tsx
@@ -26,7 +26,7 @@ export const Analyticsalerts = () => {
   const from = moment().subtract(1, 'year').startOf('quarter');
   const to = moment();
   const teamsId = {
-    teamsId: configApi.getString('opsgenie.teamsId') || DEFAULT_ALERTS_TEAMSID,}
+    teamsId: configApi.getOptionalString('opsgenie.analytics.teamsId') || DEFAULT_ALERTS_TEAMSID,};
   //const team1 = 'fd4ca533-3b2b-4629-96f8-a8884ca55e60 OR team = 33ec4c7a-b3ef-460e-92f2-55dd9b88a72c'
   //const team2 = '33ec4c7a-b3ef-460e-92f2-55dd9b88a72c'
 
