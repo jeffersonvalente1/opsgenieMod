cdd7d67f29b9bf03d97c23970fc049d0ac7afbef
jeffersonvalente1 <jefferson.valente@platformbuilders.io>
2024-07-04 09:12:28 -0300

    try 2 team alert

--- src/components/Analyticsalerts/Analyticsalerts.tsx
+++ src/components/Analyticsalerts/Analyticsalerts.tsx
@@ -25,7 +25,7 @@ export const Analyticsalerts = () => {
 
   const from = moment().subtract(1, 'year').startOf('quarter');
   const to = moment();
-  const team1 = 'fd4ca533-3b2b-4629-96f8-a8884ca55e60'
+  const team1 = 'fd4ca533-3b2b-4629-96f8-a8884ca55e60 OR team = fd4ca533-3b2b-4629-96f8-a8884ca55e60'
   //const team2 = '33ec4c7a-b3ef-460e-92f2-55dd9b88a72c'
 
   const { value: data, loading, error } = useAsync(async () => {
